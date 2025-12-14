079d63aeeaa42435a18bd6d324704ccfd7b0ed79
Bnjo42 <Mx.benijosh@gmail.com>
2025-12-14 20:46:01 +0100

    Updated HeroCarousel.tsx

--- src/components/home/HeroCarousel.tsx
+++ src/components/home/HeroCarousel.tsx
@@ -64,7 +64,7 @@ export const HeroCarousel: React.FC<HeroCarouselProps> = ({
             carouselSlides.push({
               id: trailer.id,
               videoUrl: trailer.video_url,
-              title: trailer.title,
+              title: trailer.title || trailer.tournament.title || '',
               gameName: trailer.game?.name,
               ctaText: t('heroCarousel.registerNow'),
               ctaLink: `/tournament/${trailer.tournament.id}`,
