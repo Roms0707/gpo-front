9ce1cc439f0c58e6c1c6496cb9bc204bd70f2cb0
Bnjo42 <Mx.benijosh@gmail.com>
2025-12-15 11:22:44 +0100

    Updated HeroSlide.tsx

--- src/components/home/HeroSlide.tsx
+++ src/components/home/HeroSlide.tsx
@@ -146,7 +146,7 @@ export const HeroSlide: React.FC<HeroSlideProps> = ({
         }}
       />
 
-      <div className="absolute inset-0 z-[25] flex items-center justify-center pointer-events-none pt-20 sm:pt-24 md:pt-28 lg:pt-32 pb-24 sm:pb-28 md:pb-32 lg:pb-36">
+      <div className="absolute inset-0 z-[25] flex items-center justify-center pointer-events-none pt-16 sm:pt-20 md:pt-16 lg:pt-0 pb-28 sm:pb-32 md:pb-36 lg:pb-44 xl:pb-48">
         <div className="container mx-auto px-3 sm:px-4 text-center">
           <div className="max-w-3xl mx-auto">
             {logoUrl && (
